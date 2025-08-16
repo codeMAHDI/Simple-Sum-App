f82542dd506b011ddfc18aafc32534e2b8ba2671
Mahdi <moshiurrahmanmehedi210@gmail.com>
2025-08-16 21:00:47 +0600

    Sum APP

--- lib/main.dart
+++ lib/main.dart
@@ -28,14 +28,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePage extends State<MyHomePage> {
-  Map<String, String> FormValue = {'Num1': '', 'Num2': '', 'Num3': ''};
-
+  Map<String, double> FormValue = {'Num1': 0, 'Num2': 0, 'Num3': 0};
+  double Sum=0;
   @override
   Widget build(BuildContext context) {
     MyInputOnChange(InputKey, InputValue) {
       setState(() {
-        FormValue.update(InputKey, (value)=>InputValue);
+        FormValue.update(InputKey, (value)=> double.parse(InputValue));
       });
+    }
+
+    AddAllNumber(){
+      setState(() {
+        Sum= FormValue['Num1']!+ FormValue['Num2']!+FormValue['Num3']!;
+      });
+
+
     }
 
     return Scaffold(
@@ -45,7 +53,7 @@ class _MyHomePage extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text('Sum=0', style: HeadLineText()),
+            Text(Sum.toString(), style: HeadLineText()),
             SizedBox(height: 20),
             TextFormField(
               onChanged: (value) {
@@ -72,7 +80,9 @@ class _MyHomePage extends State<MyHomePage> {
               width: double.infinity,
               child: ElevatedButton(
                 style: AppButtonStyle(),
-                onPressed: () {},
+                onPressed: () {
+                  AddAllNumber();
+                },
                 child: Text('Add'),
               ),
             ),
